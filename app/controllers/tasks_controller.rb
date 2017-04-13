7e4176b4f875425296af37b506206ebfd2285a1e
Stephane Delgado <stephanedelgado@gmail.com>
2017-04-14 01:42:39 +0800

    Top Business Software running

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,13 @@
 class TasksController < ApplicationController
 
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
+    @tasks_total = Task.count
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -13,20 +15,34 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
-    @task.save
+    @task = Task.create(task_params)
+    redirect_to tasks_path
   end
 
   def edit
-
   end
 
   def update
-
+    @task.update(task_params)
+    redirect_to task_path(@task)
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
+    redirect_to tasks_path
   end
+
+private
+
+  def task_params
+    params.require(:task).permit(:name, :content, :rating)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+
 end
+
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Task.create(name: "Read", content: "Read books about love", rating: 7)
+Task.create(name: "Travel", content: "Go travel to Finland with my love", rating: 7)
+Task.create(name: "Sing", content: "Write a song about love", rating: 7)
+Task.create(name: "Sing", content: "Sing a love song to my lover", rating: 7)
+Task.create(name: "Practice", content: "Practice makes perfect", rating: 7)
+Task.create(name: "Kubrick", content: "Too much work and no play makes Jack a dull boy", rating: 7)
