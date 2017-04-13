e87c45d964b99fc302a739ab1899faf7924e78cc
Stephane Delgado <stephanedelgado@gmail.com>
2017-04-13 21:43:54 +0800

    adding Task model

---
+++ db/migrate/20170413132915_create_tasks.rb
@@ -0,0 +1,11 @@
+class CreateTasks < ActiveRecord::Migration[5.0]
+  def change
+    create_table :tasks do |t|
+      t.string :name
+      t.string :content, default: nil
+      t.boolean :completed, default: false
+
+      t.timestamps
+    end
+  end
+end
